9e7491a8391a19154a0bef782e6457b2d0d2b0e5
Réti György <gyorgy.reti@protopmail.com>
2017-03-14 15:58:11 +0100

    Update

--- doc/index.php
+++ doc/index.php
@@ -11,6 +11,20 @@
         <script src="js/prettify.js"></script>
         <script src="js/automizy.min.js"></script>
         <script src="js/gfranko-jquery.tocify.js-c179865/src/javascripts/jquery.tocify.min.js"></script>
+        <!-- Google Tag Manager -->
+        <script>(function (w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({
+                    'gtm.start': new Date().getTime(), event: 'gtm.js'
+                });
+                var f = d.getElementsByTagName(s)[0],
+                    j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src =
+                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-NSKWRM7');</script>
+        <!-- End Google Tag Manager -->
         <script>
             var setContentWidth = function(){
                 $('#content').css({'margin-left':$('#menu-container').width()+30});
